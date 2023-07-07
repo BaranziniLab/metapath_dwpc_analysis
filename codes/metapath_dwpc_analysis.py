43d624696c38504b9364f6ed9b5f38577dba4357
Karthik Soman <karthi.soman@gmail.com>
2023-07-07 00:31:31 -0700

    parallilized the code

--- codes/metapath_dwpc_analysis.py
+++ codes/metapath_dwpc_analysis.py
@@ -5,6 +5,7 @@ import numpy as np
 import time
 from utility import *
 import os
+import multiprocessing as mp
 
 
 SOURCE_NODE_FILE = sys.argv[1]
@@ -16,7 +17,7 @@ METAGRAPH_PATH = sys.argv[6]
 GRAPH_PATH = sys.argv[7]
 SAVE_PATH = sys.argv[8]
 SAVE_NAME = sys.argv[9]
-
+NCORES = int(sys.argv[10])
 
 DAMPING_FACTOR = -0.4
 MAX_META_PATH_LENGTH = 4
@@ -34,22 +35,49 @@ extracted_metapaths = get_all_metapaths_for_node_pair(SOURCE_NODETYPE, TARGET_NO
 node_file = pd.read_csv(SOURCE_NODE_FILE)
 node_file[IDENTIFIER_COLUMN] = SOURCE_NODETYPE + ":" + node_file[IDENTIFIER_COLUMN]
 source_nodes = list(node_file[IDENTIFIER_COLUMN].unique())
-source_nodes = source_nodes[0:5]
+source_nodes = source_nodes[0:50]
 target_node = TARGET_NODETYPE + ":" + TARGET_NODE
+
 
 def main():
     start_time = time.time()
-    dwpc_for_all_source_nodes = []
-    for source_node in source_nodes:
-        dwpc_for_a_source_node = []
-        for metapath_selected in extracted_metapaths:
-            metapath_traced = get_all_paths_corresponding_to_a_metapath(source_node, target_node, metapath_selected)
-            dwpc_for_a_source_node.append(compute_dwpc_for_a_metapath(metapath_traced))
-        dwpc_for_all_source_nodes.append(dwpc_for_a_source_node)
-
-    dwpc_for_all_source_nodes = np.array(dwpc_for_all_source_nodes)
-    np.save(os.path.join(SAVE_PATH, SAVE_NAME), dwpc_for_all_source_nodes, allow_pickle=False)
+    p = mp.Pool(NCORES)
+    out_dict_list = p.map(dwpc_pipeline, source_nodes)
+    p.close()
+    p.join()
+    concatenated_dict = {}
+    for dictionary in out_dict_list:
+        concatenated_dict.update(dictionary)    
+    concatenated_dict["metapaths"] = extracted_metapaths
+    with open(os.path.join(SAVE_PATH, SAVE_NAME), "wb") as f:
+        pickle.dump(concatenated_dict, f)
     print("{} source nodes completed in {} min".format(len(source_nodes), round((time.time()-start_time)/60, 2)))
+
+
+# def main():
+#     start_time = time.time()
+#     dwpc_for_all_source_nodes = []
+#     for source_node in source_nodes:
+#         dwpc_for_a_source_node = []
+#         for metapath_selected in extracted_metapaths:
+#             metapath_traced = get_all_paths_corresponding_to_a_metapath(source_node, target_node, metapath_selected)
+#             dwpc_for_a_source_node.append(compute_dwpc_for_a_metapath(metapath_traced))
+#         dwpc_for_all_source_nodes.append(dwpc_for_a_source_node)
+
+#     dwpc_for_all_source_nodes = np.array(dwpc_for_all_source_nodes)
+#     np.save(os.path.join(SAVE_PATH, SAVE_NAME), dwpc_for_all_source_nodes, allow_pickle=False)
+#     print("{} source nodes completed in {} min".format(len(source_nodes), round((time.time()-start_time)/60, 2)))
+
+
+def dwpc_pipeline(source_node):
+    out = {}
+    dwpc_for_a_source_node = []
+    for metapath_selected in extracted_metapaths:
+        metapath_traced = get_all_paths_corresponding_to_a_metapath(source_node, target_node, metapath_selected)
+        dwpc_for_a_source_node.append(compute_dwpc_for_a_metapath(metapath_traced))
+    out[source_node] = dwpc_for_a_source_node
+    return out
+
     
 
 def compute_dwpc_for_a_metapath(metapath_traced):
