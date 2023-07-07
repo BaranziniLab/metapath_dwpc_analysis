9e5e52d9a00447876fd115202bf4ba325911777f
Karthik Soman <karthi.soman@gmail.com>
2023-07-06 23:08:07 -0700

    testing for 5 drugs

--- codes/metapath_dwpc_analysis.py
+++ codes/metapath_dwpc_analysis.py
@@ -31,7 +31,7 @@ extracted_metapaths = get_all_metapaths_for_node_pair(SOURCE_NODETYPE, TARGET_NO
 node_file = pd.read_csv(SOURCE_NODE_FILE)
 node_file[IDENTIFIER_COLUMN] = SOURCE_NODETYPE + ":" + node_file[IDENTIFIER_COLUMN]
 source_nodes = list(node_file[IDENTIFIER_COLUMN].unique())
-source_nodes = source_nodes[0:50]
+source_nodes = source_nodes[0:5]
 target_node = TARGET_NODETYPE + ":" + TARGET_NODE
 
 def main():
