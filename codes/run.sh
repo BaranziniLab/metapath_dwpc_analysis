8bc6c5250c323464bb6466cf42a5426227491f52
Karthik Soman <karthi.soman@gmail.com>
2023-07-21 21:18:44 -0700

    updated run.sh

--- codes/run.sh
+++ codes/run.sh
@@ -1,6 +1,11 @@
-conda run -n pagerank python metapath_dwpc_analysis.py ../../dwpc_data/drugs_phase_4.csv Compound GO:0070997 BiologicalProcess identifier ../../spoke_35M_data/spoke_metagraph.gpickle ../../spoke_35M_data/spoke_35M_compound_pruned_version.gpickle ../../dwpc_data/ dwpc_drug_neuron_death_bp.pickle 170 >> logs/dwpc_drug_neuron_death_bp_after_funky_edge_correction_1.log 2>&1 &
-echo "Completed neuron death BiologicalProcess"
+conda run -n pagerank python metapath_dwpc_analysis.py ../../dwpc_data/food_nodes.csv Food DOID:9352 Disease identifier ../../spoke_35M_data/spoke_metagraph.gpickle ../../spoke_35M_data/spoke_35M_compound_pruned_version.gpickle ../../dwpc_data/ dwpc_food_diabetes_disease.pickle 180 >> logs/dwpc_food_diabetes_disease_after_funky_edge_correction_1.log 2>&1 &
+echo "Completed Diabetes Disease"
 wait
+
+
+# conda run -n pagerank python metapath_dwpc_analysis.py ../../dwpc_data/drugs_phase_4.csv Compound GO:0070997 BiologicalProcess identifier ../../spoke_35M_data/spoke_metagraph.gpickle ../../spoke_35M_data/spoke_35M_compound_pruned_version.gpickle ../../dwpc_data/ dwpc_drug_neuron_death_bp.pickle 170 >> logs/dwpc_drug_neuron_death_bp_after_funky_edge_correction_1.log 2>&1 &
+# echo "Completed neuron death BiologicalProcess"
+# wait
 
 
 # conda run -n pagerank python metapath_dwpc_analysis.py ../../dwpc_data/drugs_phase_4.csv Compound GO:0042552 BiologicalProcess identifier ../../spoke_35M_data/spoke_metagraph.gpickle ../../spoke_35M_data/spoke_35M_compound_pruned_version.gpickle ../../dwpc_data/ dwpc_drug_myelination_bp.pickle 170 >> logs/dwpc_drug_myelination_bp_after_funky_edge_correction_2.log 2>&1 &
