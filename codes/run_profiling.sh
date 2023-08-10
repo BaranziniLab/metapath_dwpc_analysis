ae8ef46d946cea24c7955fe853f902e5e4121d6b
Karthik Soman <karthi.soman@gmail.com>
2023-08-10 13:31:39 -0700

    pruned metapaths in utility and amended profiling bash script

--- codes/run_profiling.sh
+++ codes/run_profiling.sh
@@ -1,3 +1,3 @@
-conda run -n pagerank python metapath_dwpc_analysis_profiling.py ../../dwpc_data/food_nodes.csv Food DOID:11476 Disease identifier '|' ../../spoke_35M_data/spoke_metagraph.gpickle ../../ncats/data/spoke_graph_ncats_version_2021_02_07.gpickle ../../dwpc_data/ dwpc_profiling_using_scaled_down_spoke.prof >> logs/dwpc_profiling_2.log 2>&1 &
+conda run -n pagerank python metapath_dwpc_analysis_profiling.py ../../dwpc_data/food_nodes.csv Food DOID:11476 Disease identifier '|' ../../spoke_35M_data/spoke_metagraph.gpickle ../../dwpc_data/spoke_ncats_version_with_more_food_nodes.gpickle ../../dwpc_data/ dwpc_profiling_using_scaled_down_spoke_with_more_food_nodes_and_pruned_metapaths.prof >> logs/dwpc_profiling_3.log 2>&1 &
 echo "Completed profiling"
 wait
--- codes/utility.py
+++ codes/utility.py
@@ -1,5 +1,60 @@
 import networkx as nx
 
+
+excluded_items = [
+    ("CONTAINS_FcC", "DOWNREGULATES_CdG", "AFFECTS_CamG", "CONTRAINDICATES_CcD"),
+    ("CONTAINS_FcC", "UPREGULATES_CdG", "AFFECTS_CamG", "CONTRAINDICATES_CcD"),
+    ("CONTAINS_FcC", "DOWNREGULATES_CdG", "AFFECTS_CamG", "TREATS_CtD"),
+    ("CONTAINS_FcC", "UPREGULATES_CdG", "AFFECTS_CamG", "CONTRAINDICATES_CcD"),
+    ("CONTAINS_FcC", "UPREGULATES_CdG", "AFFECTS_CamG", "TREATS_CtD"),
+    ("CONTAINS_FcC", "DOWNREGULATES_CdG", "UPREGULATES_CuG", "TREATS_CtD"),
+    ("CONTAINS_FcC", "DOWNREGULATES_CdG", "UPREGULATES_CuG", "CONTRAINDICATES_CcD"),
+    ("CONTAINS_FcC", "UPREGULATES_CuG", "DOWNREGULATES_CdG", "TREATS_CtD"),
+    ("CONTAINS_FcC", "UPREGULATES_CuG", "DOWNREGULATES_CdG", "CONTRAINDICATES_CcD"),
+    ('CONTAINS_FcC', 'PRODUCES_RpC', 'PARTICIPATES_PpR', 'INCREASEDIN_PiD'),
+    ('CONTAINS_FcC', 'AFFECTS_CamG', 'UPREGULATES_CuG', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'ADVRESPONSE_TO_mGarC', 'DOWNREGULATES_CdG', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'ADVRESPONSE_TO_mGarC', 'DOWNREGULATES_CdG', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'RESISTANT_TO_mGrC', 'AFFECTS_CamG', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'REDUCES_SEN_mGrsC', 'AFFECTS_CamG', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'RESPONSE_TO_mGrC', 'DOWNREGULATES_CdG', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'CONSUMES_RcC', 'PARTICIPATES_GpR', 'ASSOCIATES_DaG'),
+    ('CONTAINS_FcC', 'RESISTANT_TO_mGrC', 'UPREGULATES_CuG', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'PRODUCES_RpC', 'PARTICIPATES_GpR', 'ASSOCIATES_DaG'),
+    ('CONTAINS_FcC', 'AFFECTS_CamG', 'UPREGULATES_CuG', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'ADVRESPONSE_TO_mGarC', 'DOWNREGULATES_CdG', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'INTERACTS_PiC', 'BINDS_CbP', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'REDUCES_SEN_mGrsC', 'UPREGULATES_CuG', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'CONSUMES_RcC', 'PARTICIPATES_PpR', 'DECREASEDIN_PdD'),
+    ('CONTAINS_FcC', 'REDUCES_SEN_mGrsC', 'AFFECTS_CamG', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'RESPONSE_TO_mGrC', 'DOWNREGULATES_CdG', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'ADVRESPONSE_TO_mGarC', 'AFFECTS_CamG', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'RESPONSE_TO_mGrC', 'AFFECTS_CamG', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'REDUCES_SEN_mGrsC', 'UPREGULATES_CuG', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'UPREGULATES_CuG', 'AFFECTS_CamG', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'TRANSPORTS_PtC', 'BINDS_CbP', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'RESISTANT_TO_mGrC', 'DOWNREGULATES_CdG', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'ADVRESPONSE_TO_mGarC', 'AFFECTS_CamG', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'PRODUCES_RpC', 'PARTICIPATES_PpR', 'DECREASEDIN_PdD'),
+    ('CONTAINS_FcC', 'AFFECTS_CamG', 'DOWNREGULATES_CdG', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'RESPONSE_TO_mGrC', 'UPREGULATES_CuG', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'RESPONSE_TO_mGrC', 'AFFECTS_CamG', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'UPREGULATES_CuG', 'AFFECTS_CamG', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'TRANSPORTS_PtC', 'BINDS_CbP', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'ADVRESPONSE_TO_mGarC', 'UPREGULATES_CuG', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'RESISTANT_TO_mGrC', 'DOWNREGULATES_CdG', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'CONSUMES_RcC', 'PARTICIPATES_PpR', 'INCREASEDIN_PiD'),
+    ('CONTAINS_FcC', 'CONSUMES_RcC', 'PARTICIPATES_CpR', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'PRODUCES_RpC', 'PARTICIPATES_CpR', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'AFFECTS_CamG', 'DOWNREGULATES_CdG', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'RESPONSE_TO_mGrC', 'UPREGULATES_CuG', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'REDUCES_SEN_mGrsC', 'DOWNREGULATES_CdG', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'RESISTANT_TO_mGrC', 'AFFECTS_CamG', 'CONTRAINDICATES_CcD'),
+    ('CONTAINS_FcC', 'ADVRESPONSE_TO_mGarC', 'UPREGULATES_CuG', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'CONSUMES_RcC', 'PARTICIPATES_CpR', 'TREATS_CtD'),
+    ('CONTAINS_FcC', 'PRODUCES_RpC', 'PARTICIPATES_CpR', 'TREATS_CtD')
+]
+
 def get_all_metapaths_for_node_pair(source, target, G_metagraph, max_length=4):
     metapath_count = 0
     metapath_dict = {}
@@ -29,7 +84,15 @@ def get_all_metapaths_for_node_pair(source, target, G_metagraph, max_length=4):
                 break
 
     metapaths_refined = [elem for i, elem in enumerate(metapaths) if i not in indices_to_remove]
-    return metapaths_refined
+    metapaths_refined_2 = []
+    for item in metapaths_refined:
+        if (len(item) == 4):
+            if (item not in excluded_items):
+                if item[1] != item[2]:
+                    metapaths_refined_2.append(item)
+        else:
+            metapaths_refined_2.append(item)
+    return metapaths_refined_2
 
 
 def generate_combinations(data, current_combination=[], combinations=[]):
