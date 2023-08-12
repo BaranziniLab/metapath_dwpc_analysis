d5367014cd6cd64948769728d7efb2f77b21959d
Karthik Soman <karthi.soman@gmail.com>
2023-08-11 19:31:33 -0700

    amended bash script with loop for metapath dwpc analysis

--- codes/run_with_loop.sh
+++ codes/run_with_loop.sh
@@ -3,20 +3,20 @@ SOURCE_NODE_FILE="../../dwpc_data/food_nodes.csv"
 SOURCE_NODE_TYPE="Food"
 TARGET_NODE_TYPE="Disease"
 IDENTIFIER_COLUMN="identifier"
-NODETYPE_SEPARATOR="|"
+NODETYPE_SEPARATOR=":"
 METAGRAPH_PATH="../../spoke_35M_data/spoke_metagraph.gpickle"
-GRAPH_PATH="../../dwpc_data/spoke_ncats_version_with_more_food_nodes.gpickle"
+GRAPH_PATH="../../dwpc_data/spoke_850T.gpickle"
 SAVE_PATH="../../dwpc_data/"
 NCORES=80
 
-DISEASE_IDS=("DOID:11476" "DOID:13724" "DOID:11758")
-DISEASE_NAMES=("osteoporosis" "scurvy" "iron_deficiency_anemia")
+DISEASE_IDS=("DOID:11476" "DOID:13724" "DOID:11758" "DOID:2355")
+DISEASE_NAMES=("osteoporosis" "scurvy" "iron_deficiency_anemia" "anemia")
 
 for i in "${!DISEASE_IDS[@]}"; do
     DISEASE_ID="${DISEASE_IDS[$i]}"
     DISEASE_NAME="${DISEASE_NAMES[$i]}"
     
-    conda run -n pagerank python metapath_dwpc_analysis.py "$SOURCE_NODE_FILE" "$SOURCE_NODE_TYPE" "$DISEASE_ID" "$TARGET_NODE_TYPE" "$IDENTIFIER_COLUMN" "$NODETYPE_SEPARATOR" "$METAGRAPH_PATH" "$GRAPH_PATH" "$SAVE_PATH" "dwpc_food_${DISEASE_NAME}_disease.pickle" "$NCORES" >> "logs/dwpc_food_${DISEASE_NAME}_scaled_down_spoke_and_metapath_pruned_1.log" 2>&1 &
+    conda run -n pagerank python metapath_dwpc_analysis.py "$SOURCE_NODE_FILE" "$SOURCE_NODE_TYPE" "$DISEASE_ID" "$TARGET_NODE_TYPE" "$IDENTIFIER_COLUMN" "$NODETYPE_SEPARATOR" "$METAGRAPH_PATH" "$GRAPH_PATH" "$SAVE_PATH" "dwpc_food_${DISEASE_NAME}_disease.pickle" "$NCORES" >> "logs/dwpc_food_${DISEASE_NAME}_spoke_850T_1.log" 2>&1 &
     wait
     echo "Completed computing DWPC Food-Disease matrix for $DISEASE_NAME"
     
