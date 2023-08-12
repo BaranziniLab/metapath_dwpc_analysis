c6031e4046628df6ae18cfdbb029646562c0e059
Karthik Soman <karthi.soman@gmail.com>
2023-08-11 18:00:19 -0700

    amended profiling bash script

--- codes/run_profiling.sh
+++ codes/run_profiling.sh
@@ -1,3 +1,3 @@
-conda run -n pagerank python metapath_dwpc_analysis_profiling.py ../../dwpc_data/food_nodes.csv Food DOID:11476 Disease identifier ':' ../../spoke_35M_data/spoke_metagraph.gpickle ../../dwpc_data/spoke_850T.gpickle ../../dwpc_data/ dwpc_profiling_using_spoke850T.prof >> logs/dwpc_profiling_4.log 2>&1 &
-echo "Completed profiling"
+conda run -n pagerank python metapath_dwpc_analysis_profiling.py ../../dwpc_data/food_nodes.csv Food DOID:11476 Disease identifier ':' ../../spoke_35M_data/spoke_metagraph.gpickle ../../dwpc_data/spoke_850T.gpickle ../../dwpc_data/ dwpc_profiling_using_spoke850T.prof >> logs/dwpc_profiling_spoke_850T_1.log 2>&1 &
 wait
+echo "Completed profiling"
